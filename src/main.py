fab96437014f902afae489b040dbb48d32f13d48
Gabe Rapaport <gabrielrapaport@gmail.com>
2017-09-03 01:50:49 -0400

    The Button

--- src/main.py
+++ src/main.py
@@ -5,13 +5,22 @@ class GUI:
     def __init__(self, master):
         self.frame = Frame(master, background="dark green", highlightbackground="black", highlightcolor="black", highlightthickness=2, bd=0)
         self.frame.pack()
+        Btn_Deals = self.Btn_Deal = Button(self.frame, text = "Sup Bro", command = self.Deal)
+        Btn_Deals.place(x = 20, y=30, width = 120, height = 25)
+        Btn_Deals.pack()
+    def Deal(self):
+        print("C.A.B.")
+
+
 
 root = Tk()
 root.title("ChatBot")
-root.configure(background="brown")
+root.configure(background="green")
 
-root.minsize(height=417, width=467)
-root.maxsize(height=417, width=467)
+root.minsize(height=750, width=600)
+root.maxsize(height=750, width =600)
+
 
 k = GUI(root)
 root.mainloop()
+
